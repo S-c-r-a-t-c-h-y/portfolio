55f4b1715ac92f6676e8a46b672586a135163f4c
Scratchy <amormc74@gmail.com>
2024-07-03 22:45:50 +0200

    prettier printing

--- calcformel/lib/printing.ml
+++ calcformel/lib/printing.ml
@@ -7,24 +7,35 @@ let nb_to_string (i : float) =
   if String.ends_with ~suffix:"." s then String.sub s 0 (String.length s - 1)
   else s
 
-let rec string_of_expr (e : expr) =
+let rec string_of_expr ?(implicit_mul : bool = false) (e : expr) =
   let rec print_list (l : expr list) (sep : string) =
     match l with
     | [] -> ""
-    | [ x ] -> string_of_expr x
+    | [ x ] -> aux x false
     | x :: (Times [ Nb -1.; _ ] as y) :: xs when sep = "+" ->
-        string_of_expr x ^ print_list (y :: xs) sep
-    | x :: xs -> string_of_expr x ^ sep ^ print_list xs sep
+        aux x false ^ print_list (y :: xs) sep
+    | x :: xs -> aux x false ^ sep ^ print_list xs sep
+  and aux e encapsulate =
+    match e with
+    | Nb i -> nb_to_string i
+    | Cst c -> string_of_cst c
+    | Var i -> i
+    | Plus l ->
+        if encapsulate then "(" ^ print_list l "+" ^ ")" else print_list l "+"
+    | Times l ->
+        if encapsulate then
+          "("
+          ^ (if implicit_mul then print_list l "" else print_list l "*")
+          ^ ")"
+        else if implicit_mul then print_list l ""
+        else print_list l "*"
+    | Frac (e1, e2) ->
+        if encapsulate then "(" ^ aux e1 true ^ "/" ^ aux e2 true ^ ")"
+        else aux e1 true ^ "/" ^ aux e2 true
+    | Pow (e1, e2) -> "(" ^ aux e1 true ^ "^" ^ aux e2 true ^ ")"
+    | Func (f, e2) -> string_of_func f ^ "(" ^ aux e2 false ^ ")"
   in
-  match e with
-  | Nb i -> nb_to_string i
-  | Cst c -> string_of_cst c
-  | Var i -> i
-  | Plus l -> "(" ^ print_list l "+" ^ ")"
-  | Times l -> "(" ^ print_list l "*" ^ ")"
-  | Frac (e1, e2) -> "(" ^ string_of_expr e1 ^ "/" ^ string_of_expr e2 ^ ")"
-  | Pow (e1, e2) -> "(" ^ string_of_expr e1 ^ "^" ^ string_of_expr e2 ^ ")"
-  | Func (f, e2) -> string_of_func f ^ "(" ^ string_of_expr e2 ^ ")"
+  aux e false
 
 (* let rec latex_of_expr ?(variable_format : string = "v") (e : expr) =
    match e with
@@ -57,4 +68,5 @@ let rec string_of_expr (e : expr) =
    | Func (f, e2) ->
        latex_of_func f ^ "{" ^ latex_of_expr e2 ~variable_format ^ "}" *)
 
-let print_expr (e : expr) = e |> string_of_expr |> print_endline
+let print_expr ?(implicit_mul : bool = false) (e : expr) =
+  e |> string_of_expr ~implicit_mul |> print_endline
